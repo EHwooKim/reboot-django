6b847f1af4a3b334a3501392830dfae337ea962d
EHwooKim <ehwoo0707@naver.com>
2019-09-10 09:19:24 +0900

    오타수정

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# 내가 Article.objects 같은 정의하지 않을 것들을 쓰잖아. 상속받은게 있기 때문이고, Model은 CamelCase인걸 보니 클래스구나 
 class Article(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
